b8c19a3c3a6b8798ad728d8bb3147d33788b99ac
leewalton2014 <35839824+leewalton2014@users.noreply.github.com>
2020-02-24 02:01:39 +0000

    starting to create css layout and look of webpage

--- nclSport.sql
+++ nclSport.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS `ncl_users`;
 CREATE TABLE IF NOT EXISTS `ncl_users` (
   `userID` int(10) NOT NULL auto_increment,
-  `forename` varchar(255) default NULL,
-  `surname` varchar(400) default NULL,
+  `username` varchar(200) default NULL,
+  `forename` varchar(200) default NULL,
+  `surname` varchar(200) default NULL,
   `dob` date default NULL,
-  `email` time default NULL,
+  `email` varchar(300) default NULL,
   `gender` varchar(10) default NULL,
   `userType` varchar(10) default NULL,
   `passwordHash` varchar(100) default NULL,
@@ -17,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `ncl_users` (
 
 DROP TABLE IF EXISTS `ncl_account_type`;
 CREATE TABLE IF NOT EXISTS `ncl_account_type` (
-  `accountTypeID` varchar(10) NOT NULL default ``,
+  `accountTypeID` int(10) default NULL,
   `role` varchar(30) default NULL,
   PRIMARY KEY (`accountTypeID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -39,7 +40,8 @@ DROP TABLE IF EXISTS `ncl_facilities`;
 CREATE TABLE IF NOT EXISTS `ncl_facilities` (
   `facilityID` int(10) NOT NULL auto_increment,
   `description` varchar(100) default NULL,
-  PRIMARY KEY (`tagID`)
+  `capacity` int(10) default NULL,
+  PRIMARY KEY (`facilityID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
 
 DROP TABLE IF EXISTS `ncl_bookings`;
@@ -54,13 +56,13 @@ DROP TABLE IF EXISTS `ncl_cms_content`;
 CREATE TABLE IF NOT EXISTS `ncl_cms_content` (
   `contentID` int(10) NOT NULL auto_increment,
   `publisher` varchar(30) default NULL,
-  `publisher` varchar(30) default NULL,
-  `publisher` varchar(30) default NULL,
-  `publisher` varchar(30) default NULL,
-  `publisher` varchar(30) default NULL,
-  `publisher` varchar(30) default NULL,
-  `publisher` varchar(30) default NULL,
-  PRIMARY KEY (`tagID`)
+  `date` date default NULL,
+  `time` time default NULL,
+  `version` varchar(30) default NULL,
+  `contentTitle` varchar(300) default NULL,
+  `contentBody` varchar(5000) default NULL,
+  `contentTag` varchar(10) default NULL,
+  PRIMARY KEY (`contentID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
 
 DROP TABLE IF EXISTS `ncl_cms_tags`;
@@ -73,25 +75,31 @@ CREATE TABLE IF NOT EXISTS `ncl_cms_tags` (
 
 
 
-INSERT INTO 'ncl_users' ('userID', 'forename', 'surname', 'dob', 'email', 'gender',
-  'userType', 'passwordHash', 'membershipEXP', 'postcode', 'addrL2', 'addrL1') VALUES
-  ('1', 'Lee', 'Walton', '16/02/1999', 'lee.walton2014@outlook.com', 'Male',
-    '1', '$2y$12$AhcoeDloQQj4CPwwWqA30.SCI0ZIq1Gs3FlYeH5i4UCHA2QrQDjli', '', 'DH87RH', 'Consett', '17 New Black Street'),
-  ('1', 'Adam', 'Carr', '12/01/2000', 'admb445@gmail.com', 'Male',
-    '1', '$2y$12$AhcoeDloQQj4CPwwWqA30.SCI0ZIq1Gs3FlYeH5i4UCHA2QrQDjli', '', 'NE81AQ', 'Gateshead', '202 High Street');
+INSERT INTO ncl_users (userID, username, forename, surname, dob, email, gender,
+  userType, passwordHash, membershipEXP, postcode, addrL2, addrL1) VALUES
+  ('1', 'lee.walton', 'Lee', 'Walton', '16/02/16', 'lee.walton2014@outlook.com', 'Male',
+    '3', '$2y$12$AhcoeDloQQj4CPwwWqA30.SCI0ZIq1Gs3FlYeH5i4UCHA2QrQDjli', '', 'DH87RH', 'Consett', '17 New Black Street'),
+  ('2', 'adam.carr', 'Adam', 'Carr', '2000/01/11', 'admb445@gmail.com', 'Male',
+    '3', '$2y$12$AhcoeDloQQj4CPwwWqA30.SCI0ZIq1Gs3FlYeH5i4UCHA2QrQDjli', '', 'NE81AQ', 'Gateshead', '202 High Street'),
+  ('3', 'adamcarr1999', 'Adam', 'Carr', '2000/01/11', 'admb443@gmail.com', 'Male',
+    '2', '$2y$12$AhcoeDloQQj4CPwwWqA30.SCI0ZIq1Gs3FlYeH5i4UCHA2QrQDjli', '2029-01-20', 'NE81AQ', 'Gateshead', '202 High Street');
 
-INSERT INTO 'ncl_account_type' ('accountTypeID', 'role') VALUES
-('a1', 'user'),
-('a2', 'member'),
-('a3', 'staff'),
-('a4', 'admin');
+INSERT INTO ncl_account_type (accountTypeID, role) VALUES
+('1', 'user'),
+('2', 'member'),
+('3', 'staff'),
+('4', 'admin');
 
-INSERT INTO 'ncl_events' ('eventID', 'eventTitle', 'eventDescription', 'eventDate', 'eventTime', 'facilityID') VALUES
-('1', 'Yoga', 'The art of movement, you will learn from the start the basics and will be led by the best instructor in the NE.', '12/07/20', '15:00', '00:45', '1'),
-('1', 'Squash', 'Advanced squash, you will have access to the best squash coach to help you improove your technique.', '12/07/20', '13:00', '01:30', '2');
+INSERT INTO ncl_events (eventID, eventTitle, eventDescription, eventDate, eventTime, eventDuration, facilityID) VALUES
+('100', 'Yoga', 'The art of movement, you will learn from the start the basics and will be led by the best instructor in the NE.', '20/07/20', '15:00', '00:45', '1'),
+('101', 'Squash', 'Advanced squash, you will have access to the best squash coach to help you improove your technique.', '20/07/20', '13:00', '01:30', '2');
 
-INSERT INTO 'ncl_cms_tags' ('tagID', 'tagName') VALUES
+INSERT INTO ncl_cms_tags (tagID, tagName) VALUES
 ('1', 'index'),
 ('2', 'about'),
 ('3', 'Nutrition'),
 ('4', 'News');
+
+INSERT INTO ncl_facilities (description, capacity) VALUES
+('sports hall', '200'),
+('squash court', '10');
