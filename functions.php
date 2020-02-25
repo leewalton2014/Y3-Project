d3cc6b21db5c15110a1c6eb4be5a0bb6340de651
Lee Walton <lee.walton2014@outlook.com>
2020-02-25 14:11:09 +0000

    centered main body, fixed form css colum definitions added banner image on home page

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ function startHTML($title, $description){
   <!doctype html>
   <html lang='en'>
     <head>
-        <meta charset='UTF-8' />
+        <meta charset='UTF-8'/>
         <meta name='description' content='$description'>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <title>$title</title>
--- index.php
+++ index.php
@@ -5,12 +5,13 @@ startHTML('Newcastle Sport Home','The home of newcastles newest sport venue');
 makeNav();
 //Main Body
 echo "<div class='mainBody'>
+<img class='width100'src='assets/img/boxing.jpg'>
 <h1>Newcastle Sport</h1>
 <p>Welcome to Newcastle Sport the city centres new multi-million pound facility, with a range of
 classes and facilities for every one of any age and ability to enjoy. There is something for you
 at Newcastle Sport, so why not give us a visit.</p>
 <p>More info here.</p>
-<a href='signup-form.php'>Sign Up Today!</a>
+<a href='signup-form.php' class='big-button'>Sign Up Today!</a><br>
 </div>";
 makeFooter();
 endHTML();
