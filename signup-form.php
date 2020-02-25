5fb86065d9a57d928994af929607539f50db1019
leewalton2014 <35839824+leewalton2014@users.noreply.github.com>
2020-02-25 02:25:00 +0000

    signup form fields added next ---> create script to insert to DB

--- signup-form.php
+++ signup-form.php
@@ -8,15 +8,36 @@ echo "<div class='mainBody'>
   <h1>Sign Up</h1>
   <p>Enter your information to sign up to booking system.</p>
   <form action='login-process.php' method='POST' enctype='multipart/form-data' id='login-form'>
-    <input placeholder='Forename' type='text' name='username' class='username'/>
-    <input placeholder='Surname' type='text' name='' class=''/>
-    <input placeholder='Email' type='text' name='' class=''/>
-    <input placeholder='Username' type='text' name='' class=''/>
-    <input placeholder='Password' type='text' name='' class=''/>
-    <input placeholder='Confirm Password' type='text' name='' class=''/>
-    <input placeholder='' type='date' name='dob' class='dob'/>
-    <input placeholder='' type='text' name='' class=''/>
-    <input placeholder='' type='text' name='' class=''/>
+    <div class='col-2-width'>
+    <label for='forename'>Forename</label>
+    <input placeholder='Forename' type='text' name='forename' class='username'/>
+    <label for='surname'>Surname</label>
+    <input placeholder='Surname' type='text' name='surname'/>
+    <label for='email'>Email</label>
+    <input placeholder='Email' type='text' name='email'/>
+    <label for='username'>Username</label>
+    <input placeholder='Username' type='text' name='username'/>
+    <label for='password'>Password</label>
+    <input placeholder='Password' type='text' name='password'/>
+    <label for='passwordchk'>Confirm Password</label>
+    <input placeholder='Confirm Password' type='text' name='passwordchk'/>
+    </div>
+    <div class='col-2-width'>
+    <label for='dob'>Date Of Birth</label>
+    <input type='date' name='dob'/>
+    <label for='gender'>Gender</label>
+    <select id='gender' name='gender'>
+    <option value='Male'>Male</option>
+    <option value='Female'>Female</option>
+    </select>
+    <input type='hidden' name='userType' value='2'/>
+    <label for='addr1'>Address Line 1</label>
+    <input placeholder='Address Line 1' type='text' name='addr1'/>
+    <label for='addr2'>Address Line 2</label>
+    <input placeholder='Address Line 2' type='text' name='addr2'/>
+    <label for='postcode'>Postcode</label>
+    <input placeholder='Postcode' type='text' name='postcode'/>
+    </div>
     <input class='logInSubmit' type='submit'/><br>
     <p>Allready signed up? <a href='login-form.php'>Login</a></p>
 </div>";
