951731de1f74bf5584e70dcb461637bc6dc222bb
Lee Walton <lee.walton2014@outlook.com>
2020-03-10 13:32:29 +0000

    class timetable complete weekly view good implement next week button ++more admin features started...

--- class-timetable.php
+++ class-timetable.php
@@ -5,62 +5,57 @@ startHTML('Timetable','View classes and book classes to attend');
 makeNav();
 echo "<div class='mainBody'>";
 echo "<h1>Class Timetable</h1>";
-$currentDate = date("d/m/y");
+$currentDate = date("yy/m/d");
 $weekdays = 7;
-$getEventQuery = "SELECT eventID, eventTitle, eventDescription, eventDate, eventTime, eventDuration, description, eventBookingLimit
-FROM ncl_events INNER JOIN ncl_facilities ON ncl_events.facilityID = ncl_facilities.facilityID
-WHERE eventDate >= '$currentDate'
-GROUP BY eventDate ORDER BY eventDate, eventTime asc";
-$dbConn = getConnection();
-$queryResult = $dbConn->query($getEventQuery);
-
-/*echo "<table id='eventsTable'>
-<tr>
-<th>Event</th>
-<th>Date</th>
-<th>Time</th>
-<th>End Time</th>
-<th>Duration</th>
-<th>Location</th>
-</tr>";*/
-
-
-while ($rowObj = $queryResult->fetchObject()){
-  echo "<div class='timetable-column'>";
-  while ($weekdays > 0){
-    $weekdays = $weekdays - 1;
-    $startTime = $rowObj->eventTime;
-    $duration = $rowObj->eventDuration;
-    $endTime = $startTime + $duration;
-    $date = date_create("{$rowObj->eventDate}");
-    $date = date_format($date, "d/m/y");
-    //Day in words
-    //$day = date('', $date);
-    //echo "<div class='timetable-heading'>";
-    //echo "<h2>'$currentDate'</h2>";
-    //echo "</div>";
-    if ($currentDate == $date){
-    echo "<div class='timetable-event'>";
-    echo "<h3>{$rowObj->eventTitle}</h3>";
-    echo "<p>$date</p>";
-    echo "<p>{$rowObj->eventTime}</p>";
-    echo "<p>$endTime</p>";
-    echo "<p>{$rowObj->eventDuration}</p>";
-    echo "<p>{$rowObj->description}</p>";
+echo "<div class='timetable'>";
+while ($weekdays > 0){
+  //get todays events
+  $getEventQuery = "SELECT eventID, eventTitle, eventDescription, eventDate, eventTime, eventDuration, description, eventBookingLimit
+  FROM ncl_events INNER JOIN ncl_facilities ON ncl_events.facilityID = ncl_facilities.facilityID
+  WHERE eventDate = '$currentDate'
+  ORDER BY eventTime asc";
+  $dbConn = getConnection();
+  $queryResult = $dbConn->query($getEventQuery);
+  if ($queryResult == NULL){
+    //if no events today display blank col
+    echo "<div class='timetable-column'>";
+    echo "<div class='timetable-heading'>";
+    echo "<h2>$currentDate</h2>";
     echo "</div>";
-  }else {
-    echo "";
-    echo "";
+    echo "</div>";
+  }else{
+    echo "<div class='timetable-column'>";
+    //column heading
+    echo "<div class='timetable-heading'>";
+    echo "<h2>$currentDate</h2>";
+    echo "</div>";
+    //display events in col
+    while ($rowObj = $queryResult->fetchObject()){
+        //attempt to calculate end time
+        $startTime = $rowObj->eventTime;
+        $duration = $rowObj->eventDuration;
+        $endTime = $startTime + $duration;
+
+        //display events
+        echo "<div class='timetable-event'>";
+        echo "<h3>{$rowObj->eventTitle}</h3>";
+        echo "<p>{$rowObj->eventDescription}</p>";
+        echo "<p>{$rowObj->eventTime}</p>";
+        echo "<p>{$rowObj->eventDuration}</p>";
+        echo "<p>{$rowObj->description}</p>";
+        echo "</div>";
+    }
+    echo "</div>";//end column
   }
-  }
+$weekdays = $weekdays - 1;
+$currentDate = date('Y-m-d', strtotime($currentDate. ' + 1 days'));
 }
 
+$nextPageStartDate = $currentDate;
+echo "</div>";//timetable
+echo "<a href='class-timetable.php?weekStart=$nextPageStartDate' class='week-button'>Next Week</a><br>";
 
-
-
-
-echo "</table>";
-echo "</div>";
+echo "</div>";//main body
 makeFooter();
 endHTML();
 ?>
--- dashboard.php
+++ dashboard.php
@@ -16,6 +16,7 @@ if ($usertype >= 3){
 echo "<p>YOU SUCCESSFULLY LOGGED IN</p>";
 echo "<p>Welcome $username</p>
 <a href='newevent-form.php' class='big-button'>New Event (testing)</a><br>
+<a href='manage-events.php' class='big-button'>Manage Events (testing)</a><br>
 <a href='view-users.php' class='big-button'>View Users (testing)</a><br>";
 
 }else{
