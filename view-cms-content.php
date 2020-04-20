52a8fdd8cbf418af777c2a3f49bf40737ec6cf6a
Lee Walton <lee.walton2014@outlook.com>
2020-04-20 02:41:04 +0100

    view cms post
    Signed-off-by: Lee Walton <lee.walton2014@outlook.com>

--- view-cms-content.php
+++ view-cms-content.php
@@ -3,34 +3,31 @@ include 'functions.php';
 setSessionPath();
 startHTML('Newcastle Sport','The home of newcastles newest sport venue');
 makeNav();
-makeTitle('News and Info Articles');
-echo "<div class='mainBody'>";
+$contentID = isset($_REQUEST['contentID']) ? $_REQUEST['contentID'] : null;
 
 $dbConn = getConnection();
-
 $getCMSContent = "SELECT contentID, username, postDate, postTime, version, contentTitle, contentBody, contentTag, tagName
 FROM ncl_cms_content INNER JOIN ncl_cms_tags ON ncl_cms_content.contentTag = ncl_cms_tags.tagID
 INNER JOIN ncl_users ON ncl_cms_content.publisher = ncl_users.userID
+WHERE contentID = '$contentID'
 ORDER BY postDate desc, postTime desc";
 $CMSContent = $dbConn->query($getCMSContent);
-
-echo "<div class='content-list'>";
-while ($content = $CMSContent->fetchObject()){
-    echo "<div class='content-card'>";
-    echo "<a href='view-cms-content.php?contentID={$content->contentID}' class='big-button'>{$content->contentTitle}</a><br>";
-    echo "<p><b>#{$content->tagName}</b></p>";
-    echo "<p><b>Posted By: </b>{$content->username}</p>";
-    echo "<p><b>Published: </b>{$content->postDate} {$content->postTime}</p>";
-    echo "</div>";
-}
+$content = $CMSContent->fetchObject();
+makeTitle($content->contentTitle);
+echo "<div class='mainBody'>";
+//post
+echo "<div class='cms-post'>";
+$post = $content->contentBody;
+echo htmlspecialchars_decode((stripslashes($post)));
 echo "</div>";
-echo "<div class='tag-list'>";
-
-
-
-
+//post-info
+echo "<div class='cms-post'>";
+echo "<p><b>#{$content->tagName}</b></p>";
+echo "<p><b>Posted By: </b>{$content->username}</p>";
+echo "<p><b>Published: </b>{$content->postDate} {$content->postTime}</p>";
 echo "</div>";
 
+echo "<a href='cms.php' class='big-button'>Back</a><br>";
 echo "</div>";
 makeFooter();
 endHTML();
