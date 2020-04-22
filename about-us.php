705eabcc1324e4974d4f42fc4605714fa7062622
Lee Walton <lee.walton2014@outlook.com>
2020-04-22 02:25:23 +0100

    cms included in about us page booking process sends emails in php booking process checks for current bookings to avoid double bookings
    Signed-off-by: Lee Walton <lee.walton2014@outlook.com>

--- about-us.php
+++ about-us.php
@@ -3,8 +3,38 @@ include 'functions.php';
 setSessionPath();
 startHTML('Newcastle Sport Home','The home of newcastles newest sport venue');
 makeNav();
+makeTitle('About Us');
+echo "<div class='mainBody'>";
+echo "<h1>Location</h1>";
+echo "<iframe src='https://www.google.com/maps/embed/v1/place?key=AIzaSyD-Ikb9FiJVmyXVjSUH2Wms2ot8enQbzu0&q=Newcastle,UnitedKingdom' width='100%' height='400' frameborder='0' style='border:0;' allowfullscreen=''></iframe>";
+$tagID = "2";
+$dbConn = getConnection();
+$getCMSContent = "SELECT contentID, username, postDate, postTime, version, contentTitle, contentBody, contentTag, tagName
+FROM ncl_cms_content INNER JOIN ncl_cms_tags ON ncl_cms_content.contentTag = ncl_cms_tags.tagID
+INNER JOIN ncl_users ON ncl_cms_content.publisher = ncl_users.userID
+WHERE contentTag = '$tagID'
+ORDER BY postDate desc, postTime desc";
+
+$CMSContent = $dbConn->query($getCMSContent);
+while($content = $CMSContent->fetchObject()){
+//post
+echo "<div class='cms-post'>";
+$post = $content->contentBody;
+echo htmlspecialchars_decode((stripslashes($post)));
+echo "</div>";
+//post-info
+echo "<div class='cms-post'>";
+echo "<p><b>Last Updated: </b>{$content->postDate} {$content->postTime}</p>";
+echo "</div>";
+}
 
 
+
+
+
+
+
+echo "</div>";
 makeFooter();
 endHTML();
 ?>
--- booking-process.php
+++ booking-process.php
@@ -6,20 +6,54 @@ makeNav();
 //GET VARIABLES FROM FORM AND SANITISE USING FUNCTION IN CUSTOM SCRIPT
 $eventID = isset($_REQUEST['eventID']) ? $_REQUEST['eventID'] : null;
 $userID = $_SESSION['userID'];
+$dbConn = getConnection();
 
-//INSERT QUERY
+//Insert query
 $booking_query = "INSERT INTO ncl_bookings (eventID, userID)
 VALUES ('$eventID', '$userID')";
+//Get event and user info for email confirmation message
+$getUserInfo = "SELECT forename, surname, email
+FROM ncl_users
+WHERE userID = '$userID'";
+$getEventInfo = "SELECT eventTitle, eventDescription, eventDate, eventTime
+FROM ncl_events
+WHERE eventID = '$eventID'";
 
-$dbConn = getConnection();
-$queryResult = $dbConn->query($booking_query);
-      if ($queryResult === false) {
-        echo "<p>Please try again! <a href='class-timetable.php'>Try again.</a></p>\n";
-        exit;
-      }else{
-        header("Location: dashboard.php");
-        die();
-      }
+//Check user has not allready booked class
+$checkUserBookings = "SELECT bookingID
+FROM ncl_bookings
+WHERE eventID = '$eventID' AND userID = '$userID'";
+$checkStatus = $dbConn->query($checkUserBookings);
+
+
+$userInfo = $dbConn->query($getUserInfo);
+$eventInfo = $dbConn->query($getEventInfo);
+$user = $userInfo->fetchObject();
+$event = $eventInfo->fetchObject();
+$to = $user->email;
+$subject = "Booking Confirmation for {$event->eventTitle}";
+$message = "You have succsessfully booked {$event->eventTitle}, on {$event->eventDate} at {$event->eventTime}.";
+
+
+if ($checkStatus->rowCount() == 0){
+  //if no booking allready made by user
+  $queryResult = $dbConn->query($booking_query);
+        if ($queryResult === false) {
+          echo "<p>Please try again! <a href='class-timetable.php'>Try again.</a></p>\n";
+          exit;
+        }else{
+          //Send Email
+          mail($to,$subject,$message);
+          //Take to dashboard
+          header("Location: dashboard.php");
+          die();
+        }
+}else{
+  //booking allready made so alert user and dont create duplicate bookings
+  echo "<p>You have allready booked this class. View your bookings <a href='viewuser-bookings.php'><b>Here</b>.</a></p>\n";
+}
+
+
 makeFooter();
 endHTML();
 ?>
